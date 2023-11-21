f07f477e6e9fbcbd76d84522aa447b5fa1eb0c5d
JemyNathanael <jemynathanael0821@gmail.com>
2023-11-22 00:12:24 +0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,38 @@
 import tkinter as tk # Import the library to make a GUI
 import tkinter.messagebox
-# from tkinter.constants import SUNKEN 
+from tkinter import *
 
 # make a window for our GUI
 window = tk.Tk()
 window.title("CALCULATOR") # add the GUI title
+window.geometry("800x800")
 
-# make a frame to contains our previous window
-frame = tk.Frame(master=window, background="blue", cursor="heart", highlightcolor="yellow")
-frame.pack(); # pack the frame
 # make an Entry for user to do inputs in the calculator
-entry = tk.Entry(master=frame, background="red", highlightcolor="orange", relief="sunken", width=40, borderwidth=4)
-entry.grid(column=0, row=0)
+entry = tk.Entry(master=window, background="skyblue", relief="sunken", width=60 )
+entry.pack()
 
 def click(nums):
     entry.insert(tk.END, nums) # intert number at the end of current content
 
+def evaluateCalculation():
+    try:
+        x = str(eval(entry.get()))
+        entry.delete(0, tk.END)
+        entry.insert(0, x)
+    except:
+        tkinter.messagebox.showerror("Error", "Syntax error, please try again!")
+
+def clearCalculator():
+    entry.delete(0, tk.END)
+
+# Start creating the label for the results and buttons for user inputs
+equation_txt = ""
+equation_label = StringVar()
+result_label = Label(master=window, textvariable=equation_label, width=50, height=2, bg="blue")
+result_label.pack()
+
+# make a frame for our buttons placement using grid 
+frame = tk.Frame(master=window)
+frame.pack(); 
+
+window.mainloop()
